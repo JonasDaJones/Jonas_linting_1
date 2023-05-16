81ca7ea3e6a383b837f3ae44e6099de9c08e075f
Jonas Laß <johnny.jones@gmx.net>
2023-05-16 15:42:14 +0200

    check Renes hint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function toggleButton() {
   /*  const buttons = cards.querySelectorAll('[data-js=buttons]'); */
   cards.forEach(card => {
     const button = card.querySelector('[data-js="button"]');
-    const link = cards.querySelector('[data-js="link"]');
+    const link = card.querySelector('[data-js="link"]');
     const arrow = card.querySelector('[data-js="arrow"]');
     button.addEventListener('click', () => {
       link.classList.toggle('hide');
